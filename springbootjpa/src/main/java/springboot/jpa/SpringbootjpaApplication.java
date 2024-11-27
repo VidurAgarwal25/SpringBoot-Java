cceea131273516a22411069ca1aec58874b3f479
VidurAgarwal25 <geurecruitviduragarwal@gmail.com>
2024-11-27 21:19:56 +0530

    Database configuration for JPA

--- springbootjpa/src/main/java/springboot/jpa/SpringbootjpaApplication.java
+++ springbootjpa/src/main/java/springboot/jpa/SpringbootjpaApplication.java
@@ -2,12 +2,24 @@ package springboot.jpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import springboot.jpa.dao.UserRepository;
+import springboot.jpa.entities.User;
 
 @SpringBootApplication
 public class SpringbootjpaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootjpaApplication.class, args);
+		ApplicationContext context = SpringApplication.run(SpringbootjpaApplication.class, args);
+		UserRepository userRepository = context.getBean(UserRepository.class);
+		User user = new User();
+		user.setName("Vidur");
+		user.setCity("Meerut");
+		user.setStatus("Active");
+
+		User user1 = userRepository.save(user);
+		System.out.println(user1);
 	}
 
 }
