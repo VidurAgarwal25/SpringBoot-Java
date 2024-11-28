d389409ee38eb38f4f7633382b9de5b0c72eb48c
VidurAgarwal25 <geurecruitviduragarwal@gmail.com>
2024-11-28 12:59:52 +0530

    CRUD  ---> Read, ReadAll, Update in JPA

--- springbootjpa/src/main/java/springboot/jpa/SpringbootjpaApplication.java
+++ springbootjpa/src/main/java/springboot/jpa/SpringbootjpaApplication.java
@@ -1,6 +1,7 @@
 package springboot.jpa;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,25 +16,32 @@ public class SpringbootjpaApplication {
 	public static void main(String[] args) {
 		ApplicationContext context = SpringApplication.run(SpringbootjpaApplication.class, args);
 		UserRepository userRepository = context.getBean(UserRepository.class);
-		User user = new User();
-		user.setName("Vidur");
-		user.setCity("Meerut");
-		user.setStatus("Active");
-
+		/*
+		 * User user = new User(); user.setName("Vidur"); user.setCity("Meerut");
+		 * user.setStatus("Active");
+		 * 
+		 * User user1 = userRepository.save(user); System.out.println(user1);
+		 * 
+		 * //create User user2 = new User(); user2.setName("Agarwal");
+		 * user2.setCity("Uttar Pradesh"); user2.setStatus("Active");
+		 * 
+		 * User user3 = userRepository.save(user2); System.out.println(user3);
+		 * 
+		 * List<User> list = List.of(user,user2,user3); userRepository.saveAll(list);
+		 */
+		//read
+		Optional<User> optional = userRepository.findById(102);
+		User user = optional.get();
+		System.out.println(user);
+		
+		//read all
+		System.out.println(userRepository.findAll());
+		
+		//update
+		
+		user.setName("Vidur Agarwal");
 		User user1 = userRepository.save(user);
-		System.out.println(user1);
-		
-		//create
-		User user2 = new User();
-		user2.setName("Agarwal");
-		user2.setCity("Uttar Pradesh");
-		user2.setStatus("Active");
-		
-		User user3 = userRepository.save(user2);
-		System.out.println(user3);
-		
-		List<User> list = List.of(user,user2,user3);
-		userRepository.saveAll(list);
+		System.out.println("Updated user "+ user1);
 	}
 
 }
