218a9fc256a9700239029f2ded2a7e689ad9735f
Vidur Agarwal <geurecruitviduragarwal@gmail.com>
2024-12-02 00:40:53 +0530

    Removing static path and adding dynamic path of resources using ClassPathResource in SPringBoot

--- UploadFileSpringBoot/src/main/java/springboot/uploadfile/Helpers/FileUploadHelper.java
+++ UploadFileSpringBoot/src/main/java/springboot/uploadfile/Helpers/FileUploadHelper.java
@@ -1,16 +1,21 @@
 package springboot.uploadfile.Helpers;
 
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
 @Component
 public class FileUploadHelper {
-	public final String uploadDirectoryPath = "C:\\Users\\vidur\\Documents\\workspace-spring-tool-suite-4-4.26.0.RELEASE\\UploadFileSpringBoot\\src\\main\\resources\\static\\images";
+	public final String uploadDirectoryPath = new ClassPathResource("static/images/").getFile().getPath();
+	public FileUploadHelper() throws IOException{
+		
+	} 
 
 	public boolean uploadFile(MultipartFile file) {
 		boolean flag = false;
