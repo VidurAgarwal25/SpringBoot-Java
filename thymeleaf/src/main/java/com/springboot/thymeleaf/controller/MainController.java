9ecde4b2d5419d2dcc50ce94e6b8739a67cef1be
Vidur Agarwal <geurecruitviduragarwal@gmail.com>
2024-12-02 14:49:05 +0530

    Iteration in Thymeleaf in SpringBoot

--- thymeleaf/src/main/java/com/springboot/thymeleaf/controller/MainController.java
+++ thymeleaf/src/main/java/com/springboot/thymeleaf/controller/MainController.java
@@ -1,9 +1,11 @@
 package com.springboot.thymeleaf.controller;
 
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -16,5 +18,12 @@ public class MainController {
 		model.addAttribute("name", "Vidur Agarwal");
 		model.addAttribute("currentdate", new Date().toLocaleString());
 		return "about"; 
+	}
+	
+	@GetMapping("/multidata")
+	public String multiData(Model m) {
+		List<String> list = List.of("Vidur", "Agarwal","Vidur Agarwal");
+		m.addAttribute("names",list);
+		return "multidata";
 	}
 }
