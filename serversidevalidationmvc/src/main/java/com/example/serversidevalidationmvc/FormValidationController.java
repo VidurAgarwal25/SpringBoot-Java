91f8de10bf31d020c8773ee8646fe8d6e05a6877
Vidur Agarwal <geurecruitviduragarwal@gmail.com>
2024-12-03 23:25:34 +0530

    Adding Validation using dependecnies hibernate-validator and javax.validation which provides annotations in SpringBoot

--- serversidevalidationmvc/src/main/java/com/example/serversidevalidationmvc/FormValidationController.java
+++ serversidevalidationmvc/src/main/java/com/example/serversidevalidationmvc/FormValidationController.java
@@ -2,10 +2,14 @@ package com.example.serversidevalidationmvc;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
 import com.example.serversidevalidationmvc.entities.LoginEntity;
+
+import jakarta.validation.Valid;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -21,9 +25,13 @@ public class FormValidationController {
 	}
 	
 	@PostMapping("/submit")
-	public String postMethodName(@ModelAttribute("loginEntity") LoginEntity loginEntity) {
+	public String postMethodName(@Valid @ModelAttribute("loginEntity") LoginEntity loginEntity, BindingResult bindingResult) {
 		//TODO: process POST request
-		
+		if(bindingResult.hasErrors())
+		{
+			System.out.println(bindingResult);
+			return "form";
+		}
 		return "success";
 	}
 	
--- serversidevalidationmvc/src/main/java/com/example/serversidevalidationmvc/entities/LoginEntity.java
+++ serversidevalidationmvc/src/main/java/com/example/serversidevalidationmvc/entities/LoginEntity.java
@@ -1,9 +1,17 @@
 package com.example.serversidevalidationmvc.entities;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
 public class LoginEntity {
 
+	@NotBlank(message="User Name cannot be blank!!")
+	@Size(min=3, max=18, message="User Name must be between 3 - 18 characters")
 	private String userName;
+	
+	@Pattern(regexp = "^[a-zA-Z0-9. _%+-]+@[a-zA-Z0-9. -]+\\\\. [a-zA-Z]{2,}$",message = "Invalid Email!!")
 	private String email;
 	public String getUserName() {
 		return userName;
